5b61f05a987ae3098f699498b42b6cef6aa3ebd0
Fernando Venâncio <112507819+vnansolu@users.noreply.github.com>
2022-10-20 01:17:50 +0000

    Aprendemos a exibir de 3 maneiras diferentes

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ nome = "Fernando Venâncio"
 idade = 22
 #exibir nome que esta dentro da variavel 
 print(nome)
+
+#quando quiser exibir a frase "minha idade é" completando com a variável "idade"
+print("minha idade é "+str(idade))
+print(f"minha idade é {idade}\n")
+print("minha idade é {}".format(idade))
 ''' 
 Comentario em bloco
 
