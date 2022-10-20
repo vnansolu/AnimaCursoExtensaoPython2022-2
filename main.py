3ad68889430734a512b8db9df64f857d5c567955
Fernando Venâncio <112507819+vnansolu@users.noreply.github.com>
2022-10-20 00:52:51 +0000

    Aprendi a criar variaveis e exibí-las na tela

---
+++ main.py
@@ -0,0 +1,15 @@
+# Meu primeiro projeto Python!!! 
+#
+# print() = comando de saida
+print("Alo mundo!")
+
+#quando quiser guardar uma string (frase)
+
+nome = "Fernando Venâncio"
+idade = 22
+#exibir nome que esta dentro da variavel 
+print(nome)
+''' 
+Comentario em bloco
+
+'''
